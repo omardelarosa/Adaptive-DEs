c36e129380ca063f4187f5689decea01adc27548
Omar Delarosa <thedelarosa@gmail.com>
2020-01-04 11:52:49 -0500

    fixing __all__ declarattion

--- devo/__init__.py
+++ devo/__init__.py
@@ -1,16 +1,1 @@
-import DE as _DE
-import SHADE as _SHADE
-import LSHADE as _LSHADE
-import JADE as _JADE
-import jDE as _jDE
-import CoDE as _CoDE
-
-DE = _DE
-SHADE = _SHADE
-LSHADE = _LSHADE
-JADE = _JADE
-jDE = _jDE
-CoDE = _CoDE
-jDE = _jDE
-
 __all__ = ["DE", "SHADE", "LSHADE", "JADE", "jDE", "CoDE"]
